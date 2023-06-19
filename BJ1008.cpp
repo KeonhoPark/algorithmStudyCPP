c5e2e85229ca40b1cdf3914acaa3ff6e2e89987b
KeonhoPark <gse04063@gmail.com>
2023-06-19 23:30:27 +0900

    BJ1008

---
+++ BJ1008.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+// int main(void){
+//     long double a, b;
+
+//     cin >> a;
+//     cin >> b;
+
+//     cout << a / b;
+// }
+
+int main(void){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%.9f", a / (double)b);
+}
