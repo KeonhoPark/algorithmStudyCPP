6b0b99b8124e006deaeab0020c1354736e13bb8e
KeonhoPark <gse04063@gmail.com>
2023-06-27 18:59:01 +0900

    백준 8393

---
+++ BJ8393.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n = 0;
+    int res = 0;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        res += i;
+    }
+
+    cout << res;
+    return 0;
+}
