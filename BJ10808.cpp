59e8d8d28235fec1ecc8f4126ceac2c817cd2c6b
KeonhoPark <gse04063@gmail.com>
2023-07-09 15:55:55 +0900

    백준 10808

--- BJ10808.cpp
+++ BJ10808.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     vector<int> arr(26);
     string input;
     cin >> input;
@@ -17,3 +19,21 @@ int main(){
 
     return 0;   
 }
+
+// int func2(int arr[], int n)
+// {
+//     vector<int> ary(101);
+//     for(int a : arr){
+//         ary[a]++;
+//     }
+
+//     for(int i = 0; i <= 100; i++){
+//         if(ary[i] == 1 && ary[100-i] == 1){
+//             return 1;
+//         }
+//     }
+
+//     return 0;
+
+
+// }
