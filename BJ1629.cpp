d0a8867edd4061652864f662c54753deb705aaa5
KeonhoPark <gse04063@gmail.com>
2023-07-21 19:23:14 +0900

    백준 1251

---
+++ BJ1629.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a, b, c;
+int res;
+
+void sol(){
+    
+}
+
+int main(){
+    cin >> a >> b >> c;
+    return 0;
+}
--- BJ1926.cpp
+++ BJ1926.cpp
@@ -1,59 +1,86 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define col first
+// #define row second
+
+// int main(){
+//     cin.tie(0);
+//     ios::sync_with_stdio(false);
+
+//     int colSize, rowSize;
+//     cin >> colSize >> rowSize;
+
+//     int graph[colSize][rowSize];
+//     int visited[colSize][rowSize];
+//     fill(&visited[0][0], &visited[colSize][rowSize-1], 0);
+
+//     int cnt = 0;
+//     set<int, greater<int>> sizeSet;
+//     int dx[4] = {0, 1, 0, -1};
+//     int dy[4] = {1, 0, -1, 0};
+
+//     for(int i = 0; i < colSize; i++){
+//         for(int j = 0; j < rowSize; j++){
+//             cin >> graph[i][j];
+//         }
+//     }
+
+//     for(int i = 0; i < colSize; i++){
+//         for(int j = 0; j < rowSize; j++){
+//             if(graph[i][j] == 1 && visited[i][j] == 0){
+//                 queue<pair<int, int>> q;
+//                 int size = 1;
+//                 visited[i][j] = 1;
+//                 q.push(make_pair(i, j));
+//                 while(!q.empty()){
+//                     pair<int, int> curPos = q.front();
+//                     q.pop();
+//                     for(int k = 0; k < 4; k++){
+//                         int newCol = curPos.col + dy[k];
+//                         int newRow = curPos.row + dx[k];
+//                         if(!(0 <= newCol && newCol < colSize && 0 <= newRow && newRow < rowSize)) continue;
+//                         if(visited[newCol][newRow] == 1 || graph[newCol][newRow] == 0) continue;
+//                         size++;
+//                         visited[newCol][newRow] = 1;
+//                         q.push(make_pair(newCol, newRow));
+//                     }
+//                 }
+//                 cnt++;
+//                 sizeSet.insert(size);
+//             }
+//         }
+//     }
+
+//     cout << cnt << "\n";
+//     if(cnt == 0) cout << 0;
+//     else cout << *(sizeSet.begin());
+
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
-#define col first
-#define row second
+
+int total = 0;
+
+void print(int root){
+    if(root == 0){
+        return;
+    }
+    cout << root << "\n";
+    print(root-1);
+}
+
+void add(int root){
+    if(root == 0){
+        return;
+    }
+    total += root;
+    add(root-1);
+}
 
 int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-
-    int colSize, rowSize;
-    cin >> colSize >> rowSize;
-
-    int graph[colSize][rowSize];
-    int visited[colSize][rowSize];
-    fill(&visited[0][0], &visited[colSize][rowSize-1], 0);
-
-    int cnt = 0;
-    set<int, greater<int>> sizeSet;
-    int dx[4] = {0, 1, 0, -1};
-    int dy[4] = {1, 0, -1, 0};
-
-    for(int i = 0; i < colSize; i++){
-        for(int j = 0; j < rowSize; j++){
-            cin >> graph[i][j];
-        }
-    }
-
-    for(int i = 0; i < colSize; i++){
-        for(int j = 0; j < rowSize; j++){
-            if(graph[i][j] == 1 && visited[i][j] == 0){
-                queue<pair<int, int>> q;
-                int size = 1;
-                visited[i][j] = 1;
-                q.push(make_pair(i, j));
-                while(!q.empty()){
-                    pair<int, int> curPos = q.front();
-                    q.pop();
-                    for(int k = 0; k < 4; k++){
-                        int newCol = curPos.col + dy[k];
-                        int newRow = curPos.row + dx[k];
-                        if(!(0 <= newCol && newCol < colSize && 0 <= newRow && newRow < rowSize)) continue;
-                        if(visited[newCol][newRow] == 1 || graph[newCol][newRow] == 0) continue;
-                        size++;
-                        visited[newCol][newRow] = 1;
-                        q.push(make_pair(newCol, newRow));
-                    }
-                }
-                cnt++;
-                sizeSet.insert(size);
-            }
-        }
-    }
-
-    cout << cnt << "\n";
-    if(cnt == 0) cout << 0;
-    else cout << *(sizeSet.begin());
-
-    return 0;
+    print(5);
+    add(5);
+    cout << total;
 }
