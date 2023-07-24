1e11607c34c0f656cfeeb253c9d47151053a0c86
KeonhoPark <gse04063@gmail.com>
2023-07-25 00:01:17 +0900

    백준 1629

--- BJ1629.cpp
+++ BJ1629.cpp
@@ -1,14 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int a, b, c;
-int res;
+long long sol(int a, int b, int c){
+    if(b == 1) return a % c;
 
-void sol(){
-    
+    else{
+        long long temp = sol(a, b/2, c);
+        temp = temp * temp % c;
+
+        if(b % 2 == 0) return temp;
+        else{
+            return temp * a % c;
+        }
+    }
 }
 
 int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    long long a, b, c;
     cin >> a >> b >> c;
+    cout << sol(a, b, c);
     return 0;
 }
