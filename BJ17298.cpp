1304ff2ed2f9b7bbf2913d5282556b387d35923a
KeonhoPark <gse04063@gmail.com>
2023-08-18 00:08:05 +0900

    백준 17298

--- BJ17298.cpp
+++ BJ17298.cpp
@@ -1,13 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int numbers[1000001];
+int result[1000001];
+
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
     int n;
-    int numbers[1000001];
-    int result[1000001];
     memset(result, -1, sizeof(int) * 1000001);
     stack<pair<int, int>> s;
     
