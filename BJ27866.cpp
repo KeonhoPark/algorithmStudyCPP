557fdbd51003475ad93b3cdb2be74d56ebad535b
KeonhoPark <gse04063@gmail.com>
2023-09-03 23:48:10 +0900

    백준 27866

---
+++ BJ27866.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    string input;
+    int n;
+    cin >> input;
+    cin >> n;
+    cout << input[n-1];
+
+    return 0;
+}
