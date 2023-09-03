1d85ae68560508e904a69ebc575d9c8adae5b430
KeonhoPark <gse04063@gmail.com>
2023-09-03 23:45:17 +0900

    백준 25083

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,10 @@
-s.pop();
+for(int i = 0; i < a.size(); i++){
+    //     for(int j = 0; j < temp.size(); j++){
+    //         if((a[i] == temp[j]) && (visited[j] == 0)){
+    //             cout << j;
+    //             if(i != a.size() - 1) cout << " ";
+    //             visited[j] = 1;
+    //             break;
+    //         }
+    //     }
+    // }
