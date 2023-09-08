2e4f5134dbbe7aff308b1170c84ea4239f7c6cae
KeonhoPark <gse04063@gmail.com>
2023-09-08 19:12:23 +0900

    백준 1927

--- BJ1927.cpp
+++ BJ1927.cpp
@@ -3,19 +3,20 @@ using namespace std;
 
 int n;
 priority_queue<int, vector<int>, greater<int>> pq;
+vector<int> m;
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
     cin >> n;
+
     for(int i = 0; i < n; i++){
         int input;
         cin >> input;
         if(input == 0){
             if(pq.empty()){
                 cout << 0 << "\n";
-                continue;
             }
             else{
                 cout << pq.top() << "\n";
