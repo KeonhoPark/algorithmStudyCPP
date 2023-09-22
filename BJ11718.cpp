eb35f8017e25fa5d7fa5969a99f9161a8ba287da
KeonhoPark <gse04063@gmail.com>
2023-09-22 09:02:19 +0900

    백준 11718

---
+++ BJ11718.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    string input;
+    while(true){
+        getline(cin, input);
+        if(input == "") break;
+        cout << input << "\n";           
+    }
+
+    return 0;
+}
