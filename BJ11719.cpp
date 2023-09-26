b6d19922f1288e7d57b393d07f793a6c3ae298aa
KeonhoPark <gse04063@gmail.com>
2023-09-26 10:06:20 +0900

    백준 11719

---
+++ BJ11719.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    string input;
+    while(getline(cin, input)){
+        cout << input << "\n";
+    }
+
+    return 0;
+}
