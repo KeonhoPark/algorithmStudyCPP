19f7c99e166325573ca7624039720ae31334a484
KeonhoPark <gse04063@gmail.com>
2023-10-12 19:35:17 +0900

    백준 9656

---
+++ BJ9656.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    cin >> n;
+    if(n % 2 == 0) cout << "SK";
+    else cout << "CY";
+
+    return 0;
+}
